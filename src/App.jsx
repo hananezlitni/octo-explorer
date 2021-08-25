50d9500cd8395ddefe42fc945b72fd56194f6e2d
Hanane Zlitni <hananzlitni@hotmail.com>
2021-08-25 11:58:51 -0400

    Initial commit ✨

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SearchGit</h1>
+      </header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
