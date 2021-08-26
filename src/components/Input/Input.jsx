02d4f2438d3e05268e5f6f8da4f932284431d06c
Hanane Zlitni <hananzlitni@hotmail.com>
2021-08-26 18:37:40 -0400

    Style input field

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Input.module.css";
 
 const Input = ({ placeholder, forwardedRef }) => {
   return (
     <input
-      className="input"
+      className={styles.input}
       type="text"
       placeholder={placeholder}
       ref={forwardedRef}
